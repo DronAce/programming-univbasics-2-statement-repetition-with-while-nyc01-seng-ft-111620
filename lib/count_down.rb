573b1360f9970e6776a18c2f1a80dce0e7fbaf0e
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-22 16:20:47 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -2,10 +2,8 @@
 count = 10
 zero = 0
 while (true) do
-  if break count == zero
-
-    puts "Happy New Year!"
-  else
-    puts "#{count}"
+  puts "#{count}"
   count --
+  if break count == zero
+    puts "Happy New Year!"
 end
